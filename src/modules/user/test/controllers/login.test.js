d6239d629c87ea049afce3b50bc0c598cfcbe64f
kushkaushik271 <kush.kaushik@unthinkable.co>
2025-03-25 14:12:16 +0530

    jest

--- src/modules/user/test/controllers/login.test.js
+++ src/modules/user/test/controllers/login.test.js
@@ -1,37 +1,60 @@
-const express = require("express");
-const userRoutes = require("../../routes");
+const nock = require('nock');
+const request = require('supertest');
+//jo jo real me use kiya usse import kro first
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const UserService = require('../../../user/service/userService');
+const redisClient = require('../../../../lib/redisClient');
 
-jest.mock("../../service/userService");
-jest.mock("../../../../lib/redisClient");
-jest.mock("bcrypt");
-jest.mock("../../controller/userController.js");
-jest.mock("axios");
 
-const app = express();
-app.use(express.json());
-app.use("/api/users", userRoutes);
+//jaha jaha se jo real me use kiya bo aaya h usse mock kro 
+jest.mock('../../../user/service/userService.js');
+jest.mock('jsonwebtoken');
+jest.mock('bcrypt');
+jest.mock('../../../../lib/redisClient');
 
-describe("User Login Controller", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-  it("should print on console", () => {
-    // eslint-disable-next-line no-console
-    console.log("34567890");
-  });
-  it("should return 200 with login successfull message", async () => {
-    const userDetails = {
-      userId: "5403ffdb-1ee6-417b-89b5-7df99438c1e8",
-      username: "kushkaushik271",
-      useremail: "test@yopmail.com",
-      password: "$2b$10$gExqK1J.bgiT0BK8qcwkO.b3OPEEJMYg4.HcmI2icTeng.9e2RQIm",
-      isEmailVerified: true,
-      isActive: true,
-      created_at: "2025-03-10T10:43:09.973Z",
-      updated_at: "2025-03-10T10:43:09.973Z",
-      createdBy: null,
-      modifiedBy: null,
-      is_verified: true,
-    };
+const user = {
+  userId: 'd549b39b-3878-4bbb-9f4c-5ba5c0064672',
+  username: 'testUser',
+  useremail: 'test@user.com',
+  isEmailVerified: true,
+  isActive: true,
+  is_verified: true
+};
+
+const token = {
+  accessToken: 'testaccesstoken',
+  refreshToken: 'testrefreshtoken',
+};
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('loginUser', () => {
+  it.only('should login successfully with valid credentials', async () => {
+    const req = { body: { email: 'testUser', password: 'test@12345' } };
+
+    //mock nactual function result
+    bcrypt.compare.mockResolvedValue(true);
+    jwt.sign.mockReturnValueOnce(token.accessToken).mockReturnValueOnce(token.refreshToken);
+    UserService.loginUser.mockResolvedValue(req.body);
+
+    redisClient.SET.mockResolvedValue();
+
+    //here intercepts the API request made by supertest and returns a fake response.
+    nock('http://localhost:3000')
+      .post('/user/login')
+      .reply(200, { message: 'Login successful', token });
+
+    //make api call
+    await request('http://localhost:3000')
+      .post('/user/login')
+      .send(req.body)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('message', 'Login successful');
+        expect(response.body).toHaveProperty('token');
+      });
   });
 });
